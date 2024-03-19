fa98d67e6905550f827caea3cdec6eaf5e3e11a9
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2024-03-19 17:14:00 -0500

    Added example

---
+++ Example/Example1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3D
+{
+    internal class Example1
+    {
+        public void Execute()
+        {
+            bool continueFlag = true;
+            while (continueFlag)
+            {
+                Console.WriteLine("Introducir el número de la operación");
+                Console.WriteLine("1. Suma");
+                Console.WriteLine("2. Resta");
+                Console.WriteLine("0. Salir");
+                string option= Console.ReadLine();
+                switch(option)
+                {
+                    case "1":
+                        Addition();
+                        break;
+                    case "2":
+                        Subtraction();
+                        break;
+                    case "0":
+                        continueFlag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida");
+                        break;
+                }
+
+            }
+        }
+
+        private void Addition()
+        {
+            int a;
+            int b;
+            Console.WriteLine("Introduce el primer número");    
+            a= int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo número");
+            b= int.Parse(Console.ReadLine());
+            Console.WriteLine($"El resultado es: {a+b}");
+        }
+        private void Subtraction()
+        {
+            int a;
+            int b;
+            Console.WriteLine("Introduce el primer número");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo número");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"El resultado es: {a - b}");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,8 +8,12 @@ namespace Taller3D
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            Example1 e = new Example1();
+            e.Execute();
         }
+
     }
 }
