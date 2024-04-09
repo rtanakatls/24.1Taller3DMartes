78d4d3dec2e197c7d41701d745bf2393d8ee7779
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2024-04-09 16:39:26 -0500

    Added EnemyMelee

---
+++ Week4/EnemyMelee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3D.Week4
+{
+    internal class EnemyMelee : Character
+    {
+        public EnemyMelee(string name, int life, int damage)
+        {
+            this.name = name;
+            this.life = life;
+            this.damage = damage;
+        }
+    }
+}
