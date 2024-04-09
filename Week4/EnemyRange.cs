54207882ee21994f65f9b701cb0b277e87cfa6d3
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2024-04-09 16:42:13 -0500

    Added EnemyRange

---
+++ Week4/EnemyRange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3D.Week4
+{
+    internal class EnemyRange : Character
+    {
+        private int bullets;
+
+        public EnemyRange(string name, int life, int damage, int bullets)
+        {
+            this.name = name;
+            this.life = life;
+            this.damage = damage;
+            this.bullets = bullets;
+        }
+
+        
+
+    }
+}
