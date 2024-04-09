d174964b83f2500f1ef07f1d31c2ae4c45b08afc
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2024-04-09 16:37:07 -0500

    Added character

---
+++ Week4/Character.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3D.Week4
+{
+    internal class Character
+    {
+        protected string name;
+        protected int life;
+        protected int damage;
+
+
+        public virtual void TakeDamage(int damage)
+        {
+            life -= damage;
+        }
+
+        public virtual float GetDamage()
+        {
+            return damage;
+        }
+
+        public virtual string GetData()
+        {
+            return $"Name: {name} - Life: {life} - Damage: {damage}";
+        }
+
+        public virtual bool IsAlive()
+        {
+            return life <= 0;
+        }
+
+
+    }
+}
---
+++ Week4/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3D.Week4
+{
+    internal class Player
+    {
+
+
+    }
+}
